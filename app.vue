c4ebfc4c6f8206be15024b0a679cbc2b288742f7
nizarsaidicoder <nizar.saidi.coder@gmail.com>
2024-08-22 15:17:26 +0200

    Initialised the project

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NavigationBar />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ components/NavigationBar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  const route = useRoute();
+
+  const links = [
+    {
+      label: "Profile",
+      avatar: {
+        src: "https://avatars.githubusercontent.com/u/739984?v=4",
+      },
+      badge: 100,
+    },
+    {
+      label: "Home",
+      icon: "i-heroicons-home",
+    },
+    {
+      label: "Stages",
+      icon: "i-heroicons-chart-bar",
+      to: `${
+        route.path.startsWith("/dev") ? "/dev" : ""
+      }/components/horizontal-navigation`,
+    },
+  ];
+</script>
+
+<template>
+  <UHorizontalNavigation
+    :links="links"
+    class="border-b border-gray-200 dark:border-gray-800" />
+</template>
