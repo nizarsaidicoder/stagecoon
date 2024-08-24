9afad5dd8a11c07dd3d4d4357dd3dd96cd8590bf
nizarsaidicoder <nizar.saidi.coder@gmail.com>
2024-08-24 16:44:56 +0200

    Fixed the button to be always on the end

--- components/Internship/InternshipCard.vue
+++ components/Internship/InternshipCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-white text-black rounded-xl flex flex-col items-start gap-3 border-white border-2 pt-2 pb-6 px-2 max-w-[340px] card relative">
+    class="bg-white text-black rounded-xl flex flex-col items-start justify-between gap-3 border-white border-2 pt-2 pb-6 px-2 max-w-[340px] card relative">
     <div
       class="rounded-full bg-white shadow-lg flex items-center justify-center absolute top-5 right-5 p-2 z-10 cursor-pointer"
       @mouseover="hovered = true"
@@ -11,19 +11,24 @@
         class="text-lg duration-200 ease-in-out"
         :class="{ 'text-primary-500': hovered || favorite }" />
     </div>
-    <img
-      :src="data.image"
-      alt="Internship"
-      class="w-full h-[80%] object-cover rounded-xl" />
-    <div class="px-2 flex flex-col gap-1">
-      <h2 class="text-xl font-bold">{{ data.title || "Non renseigné" }}</h2>
-      <p class="text-gray-700">
-        {{ computedText || "Non renseigné" }}
-        <span class="font-bold italic">...</span>
-      </p>
+    <div class="flex flex-col gap-2">
+      <div class="w-full zoom-image">
+        <img
+          :src="data.image"
+          alt="Internship"
+          class="object-cover rounded-xl" />
+      </div>
+
+      <div class="px-2 flex flex-col gap-1">
+        <h2 class="text-xl font-bold">{{ data.title || "Non renseigné" }}</h2>
+        <p class="text-gray-700">
+          {{ computedText || "Non renseigné" }}
+          <span class="font-bold italic">...</span>
+        </p>
+      </div>
     </div>
     <UButton
-      class="self-end mr-2"
+      class="self-end justify-self-end mr-2"
       icon="i-heroicons-arrow-right-16-solid"
       color="primary"
       label="Voir plus"
@@ -58,8 +63,19 @@
   .card img {
     transition: all 01s;
   }
-  .card:hover img {
-    /* zoom */
-    scale: 1.02;
+  .card:hover .zoom-image img {
+    width: 100%;
+    -webkit-transform: scale(1.3);
+    transform: scale(1.3);
+    -webkit-transition: 0.3s ease-in-out;
+    transition: 0.3s ease-in-out;
+  }
+
+  .zoom-image {
+    overflow: hidden;
+  }
+  .zoom-image img {
+    width: 100%;
+    height: auto;
   }
 </style>
