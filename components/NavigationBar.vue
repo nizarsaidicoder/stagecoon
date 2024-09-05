1e424d10f21c5882806584e9f4794f3d0a30e151
nizarsaidicoder <nizar.saidi.coder@gmail.com>
2024-09-05 17:43:33 +0200

    Added index page + alert

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   const route = useRoute();
-
+  const open = ref(true);
   const links = [
     {
       label: "Home",
@@ -34,4 +34,18 @@
   <UHorizontalNavigation
     :links="links"
     class="border-b border-primary-200 dark:border-primary-500 z-100" />
+  <div
+    v-if="open"
+    class="bg-primary-500 py-2 text-center font-medium text-lg flex items-center justify-center relative">
+    <div class="flex items-center gap-2">
+      <UIcon name="i-heroicons-information-circle" /> Le site est toujours en
+      construction et pas indicative du produit finale, si vous rencontrer un
+      bug, veuillez le signaler dans github.
+    </div>
+
+    <UIcon
+      @click="open = false"
+      name="i-heroicons-x-circle-20-solid"
+      class="absolute top-1/2 -translate-y-1/2 right-8 w-7 h-7 cursor-pointer hover:bg-red-500" />
+  </div>
 </template>
